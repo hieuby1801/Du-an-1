1485e3be60da328b761e6cc303280414afa2de21
Phi Hoàng Hồ Kỳ <hokyhoang2310@gmail.com>
2024-07-13 10:15:44 +0700

    cậpn nhập from đăng nhập

---
+++ PhanMemQLTraSua/PhanMemQLTraSua/PhanMemQLTraSua/FrmDangNhap.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PhanMemQLTraSua
+{
+    partial class FrmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltaikhoang = new System.Windows.Forms.Label();
+            this.lblmatkhau = new System.Windows.Forms.Label();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.txttaikhoang = new System.Windows.Forms.TextBox();
+            this.llbllienhe = new System.Windows.Forms.LinkLabel();
+            this.llblhuongdan = new System.Windows.Forms.LinkLabel();
+            this.rdohienmatkhau = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbltaikhoang
+            // 
+            this.lbltaikhoang.AutoSize = true;
+            this.lbltaikhoang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbltaikhoang.Location = new System.Drawing.Point(360, 145);
+            this.lbltaikhoang.Name = "lbltaikhoang";
+            this.lbltaikhoang.Size = new System.Drawing.Size(103, 25);
+            this.lbltaikhoang.TabIndex = 0;
+            this.lbltaikhoang.Text = "Tài Khoản";
+            // 
+            // lblmatkhau
+            // 
+            this.lblmatkhau.AutoSize = true;
+            this.lblmatkhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblmatkhau.Location = new System.Drawing.Point(360, 223);
+            this.lblmatkhau.Name = "lblmatkhau";
+            this.lblmatkhau.Size = new System.Drawing.Size(97, 25);
+            this.lblmatkhau.TabIndex = 1;
+            this.lblmatkhau.Text = "Mật Khẩu";
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(494, 220);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(197, 26);
+            this.txtmatkhau.TabIndex = 2;
+            // 
+            // txttaikhoang
+            // 
+            this.txttaikhoang.Location = new System.Drawing.Point(494, 142);
+            this.txttaikhoang.Name = "txttaikhoang";
+            this.txttaikhoang.Size = new System.Drawing.Size(197, 26);
+            this.txttaikhoang.TabIndex = 3;
+            // 
+            // llbllienhe
+            // 
+            this.llbllienhe.AutoSize = true;
+            this.llbllienhe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.llbllienhe.Location = new System.Drawing.Point(401, 355);
+            this.llbllienhe.Name = "llbllienhe";
+            this.llbllienhe.Size = new System.Drawing.Size(72, 22);
+            this.llbllienhe.TabIndex = 4;
+            this.llbllienhe.TabStop = true;
+            this.llbllienhe.Text = "Liên Hệ";
+            // 
+            // llblhuongdan
+            // 
+            this.llblhuongdan.AutoSize = true;
+            this.llblhuongdan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.llblhuongdan.Location = new System.Drawing.Point(637, 355);
+            this.llblhuongdan.Name = "llblhuongdan";
+            this.llblhuongdan.Size = new System.Drawing.Size(101, 22);
+            this.llblhuongdan.TabIndex = 5;
+            this.llblhuongdan.TabStop = true;
+            this.llblhuongdan.Text = "Hướng Dẫn";
+            // 
+            // rdohienmatkhau
+            // 
+            this.rdohienmatkhau.AutoSize = true;
+            this.rdohienmatkhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rdohienmatkhau.Location = new System.Drawing.Point(494, 275);
+            this.rdohienmatkhau.Name = "rdohienmatkhau";
+            this.rdohienmatkhau.Size = new System.Drawing.Size(164, 24);
+            this.rdohienmatkhau.TabIndex = 6;
+            this.rdohienmatkhau.TabStop = true;
+            this.rdohienmatkhau.Text = "Hiển Thị Mật Khẩu";
+            this.rdohienmatkhau.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PhanMemQLTraSua.Properties.Resources.anh_ly_tra_sua_cute_hinh_nen_tra_sua_dep_nhat_03;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(317, 287);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(486, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 46);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Đăng Nhập";
+            // 
+            // FrmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rdohienmatkhau);
+            this.Controls.Add(this.llblhuongdan);
+            this.Controls.Add(this.llbllienhe);
+            this.Controls.Add(this.txttaikhoang);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.lblmatkhau);
+            this.Controls.Add(this.lbltaikhoang);
+            this.Name = "FrmDangNhap";
+            this.Text = "FrmDangNhap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltaikhoang;
+        private System.Windows.Forms.Label lblmatkhau;
+        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.TextBox txttaikhoang;
+        private System.Windows.Forms.LinkLabel llbllienhe;
+        private System.Windows.Forms.LinkLabel llblhuongdan;
+        private System.Windows.Forms.RadioButton rdohienmatkhau;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
