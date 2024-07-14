797697cb9e391c9d868d541ecf53ab944f1e8a5d
Administrator <Administrator@WINDOWS-10>
2024-07-14 14:22:39 +0700

    up frmHD

---
+++ PhanMemQLTraSua/PhanMemQLTraSua/PhanMemQLTraSua/FrmHuongDan.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PhanMemQLTraSua
+{
+    partial class FrmHuongDan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.llblFileHD = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vui lòng tải file hướng dẫn";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.llblFileHD);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(23, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(516, 42);
+            this.panel1.TabIndex = 1;
+            // 
+            // llblFileHD
+            // 
+            this.llblFileHD.AutoSize = true;
+            this.llblFileHD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llblFileHD.Location = new System.Drawing.Point(217, 11);
+            this.llblFileHD.Name = "llblFileHD";
+            this.llblFileHD.Size = new System.Drawing.Size(58, 20);
+            this.llblFileHD.TabIndex = 1;
+            this.llblFileHD.TabStop = true;
+            this.llblFileHD.Text = "tại đây";
+            // 
+            // FrmHuongDan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 110);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmHuongDan";
+            this.Text = "Hướng Dẫn";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel llblFileHD;
+    }
+}
